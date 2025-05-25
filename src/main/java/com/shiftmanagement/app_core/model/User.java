8c9be01fb4e34a26c2772e039097b71375a2d249
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-05-25 12:54:15 -0500

    Eliminacion de edad

--- src/main/java/com/shiftmanagement/app_core/model/User.java
+++ src/main/java/com/shiftmanagement/app_core/model/User.java
@@ -3,4 +3,4 @@ package com.shiftmanagement.app_core.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record User(String userName,int age, String id, String role, String password) {}
+public record User(String userName, String id, String role, String password) {}
